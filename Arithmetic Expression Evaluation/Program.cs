0281c2b5e4a58dbf4a8270118945e721892dfa71
Gaber <youssefmohamed430@gmail.com>
2024-11-18 09:42:03 +0200

    Handle the format

--- Arithmetic Expression Evaluation/Program.cs
+++ Arithmetic Expression Evaluation/Program.cs
@@ -10,12 +10,19 @@ namespace Arithmetic_Expression_Evaluation
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Enter your Expression : ");
             string str = Console.ReadLine();
             InfixToPostfixConversion inf = new InfixToPostfixConversion();
             string exp = inf.convertion(str);
             Evaluation ev = new Evaluation(exp);
-            Console.WriteLine(exp);
-            Console.WriteLine(ev.Result());
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("\nPostfix Expression : ");
+            Console.WriteLine(exp+"\n");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("The Result is : ");
+            Console.WriteLine(ev.Result()+"\n");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
